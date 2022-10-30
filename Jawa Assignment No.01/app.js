513d5da9b8ffd16c15fb299581143df3356eb833
MrHaMzUuu <115740610+MrHaMzUuu@users.noreply.github.com>
2022-10-31 00:57:18 +0500

    initial commit

---
+++ Jawa Assignment No.01/app.js
@@ -0,0 +1,107 @@
+alert("Thanks for your input!");
+
+window.alert("Thanks for your input!");
+
+var myName = "Mark";
+alert(myName);
+
+var x
+x = "Ace";
+alert(x);
+
+var nationality;
+nationality = "PK";
+alert(nationality);
+
+var floogle = "Mark";
+alert(floogle);
+
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+alert(lessonAuthor);
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+var weight = 150;
+var weight2 = weight + 25;
+alert(weight2);
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+alert(newNum);
+
+var originalNum = 23; 
+var numToBeAdded = 7; 
+var newNum = originalNum + numToBeAdded;
+alert(newNum);
+
+var originalNumber = 23;
+var newNumber = originalNumber + 7;
+alert(newNum);
+
+//var 1stPresident = "Washington";  illegal
+var prezWhoCame1st = "Washington"; //legal
+alert(prezWhoCame1st);
+
+var popularNumber = 4;
+alert(popularNumber);
+
+var popularNumber = 2 + 2;
+alert(popularNumber);
+
+alert(2 + 2);
+
+var num = 10;
+var popularNumber = num + 200;
+alert(popularNumber);
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+alert(popularNumber);
+
+var whatsLeftOver = 10 % 3;
+alert(whatsLeftOver);
+var whatsLeftOver = 9 % 3;
+alert(whatsLeftOver);
+
+var num = 1;
+var newNum = num++;
+alert(newNum);
+
+var num = 1;
+var newNum = ++num;
+alert(newNum);
+
+var num = 1;
+var newNum = num--;
+alert(newNum);
+var num = 1;
+var newNum = --num;
+alert(newNum);
+
+var totalCost = 1 + 3 * 4;
+alert(totalCost);
+
+var totalCost = (1 + 3) * 4;
+alert(totalCost);
+
+var resultOfComputation = (2 * 4) * 4 + 2;
+alert(resultOfComputation);
+
+var resultOfComputation = ((2 * 4) * 4) + 2;
+
+alert("Thanks for your input!");
+
+var message = "Thanks for your input!";
+alert(message);
+
+alert("Thanks, " + "Susan" + "!");
+
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
